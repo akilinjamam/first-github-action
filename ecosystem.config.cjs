5034ff253d65e0bb4f254ff449cec0a3f52330b7
akilinjamam <akilinjamam@gmail.com>
2025-07-12 20:00:46 +0600

    file-updated

---
+++ ecosystem.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [
+        {
+            name: "upload-server",
+            cwd: "/home/ubuntu/applications/aws-media-file-provider-server",
+            script: "./dist/server.js",
+        },
+    ],
+};
